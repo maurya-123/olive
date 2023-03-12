11204cb00bea316d8e99bf6e66d82de73fc5ccda
maurya-123 <124710510+maurya-123@users.noreply.github.com>
2023-03-12 23:08:26 +0530

    match

---
+++ js/flex.js
@@ -0,0 +1,10 @@
+function mymenu(){
+    var h = document.getElementById("menu");
+
+    if(h.style.display == "block"){
+        h.style.display = "none";
+    }
+    else{
+        h.style.display = "block"
+    }
+}
